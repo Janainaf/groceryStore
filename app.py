c5717d7f03b2e7c9e2e736c56ef389bbb2071b72
Janaina <figueira.janaina@gmail.com>
2022-04-22 22:51:45 +0200

    added menu

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ from models import Base, session, Product, Brands, engine
 import datetime
 import csv
 
+
+def menu():
+    while True:
+        print('''
+            \nWelcome to My Store Inventory
+            \n ****Here are your choices ****
+            \nN) Add New Product
+            \rV) View a Product by ID
+            \rA) Product Analysis
+            \rB) Backup the Database
+            \rQ) Quit the Application
+            ''')
+        input('What would you like to do? ')
+
 def add_csv():
     with open('brands.csv') as csvfile:
         data_brands = csv.reader(csvfile)
@@ -12,10 +26,13 @@ def add_csv():
         for row in data_products:
             print(row)
 
+def clean_data():
+    print("hello")
 
 def app():
     print("hello")
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    add_csv()
+    menu()
+    # add_csv()
