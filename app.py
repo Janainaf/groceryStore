ba19f90f003d0199269ceb2d33d46afe87fd5b5d
Janaina <figueira.janaina@gmail.com>
2022-04-22 22:42:33 +0200

    testing reading csv files

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 from models import Base, session, Product, Brands, engine
+import datetime
+import csv
+
+def add_csv():
+    with open('brands.csv') as csvfile:
+        data_brands = csv.reader(csvfile)
+        for row in data_brands:
+            print(row)
+    with open('inventory.csv') as csvfile:
+        data_products = csv.reader(csvfile)
+        for row in data_products:
+            print(row)
+
+
+def app():
+    print("hello")
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    add_csv()
