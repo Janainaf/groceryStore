3757d31e919c733d500e5177d878577c72f96829
Janaina <figueira.janaina@gmail.com>
2022-04-23 21:14:54 +0200

    added search for product

--- app.py
+++ app.py
@@ -25,13 +25,32 @@ def menu():
             \rPlease choose one of the option above
             \rPress Enter to try again''')
 
-            # ****************************** CLEANS DATA  **************************************
+ # ****************************** CLEAN DATA  **************************************
 
 def clean_price(price_string):
-    split_price= price_string.split('$')
-    float_price = float(split_price[1])
-    int_price =  int(float_price * 100)
-    return int_price
+    try:
+        split_price= price_string.split('$')
+        float_price = float(split_price[1])
+        int_price =  int(float_price * 100)
+    except:
+            input('''
+                \n *** PRICE ERROR *** 
+                \n The price should be a number with a currency symbol $ 
+                \rPress Enter to try again''')
+    else:
+        return int_price
+
+
+def clean_quantity(quantity_string):
+    try:
+        quantity_integer = int(quantity_string)
+    except ValueError:
+        input('''
+                \n *** QUANTITY ERROR *** 
+                \n The quantity should be a number  
+                \rPress Enter to try again''')
+    else:
+        return quantity_integer
 
 def clean_date(date_str):
     split_date = date_str.split('/')
@@ -41,19 +60,36 @@ def clean_date(date_str):
     final_date = datetime.date(year, month, day)
     return final_date
 
-def clean_quantity(quantity_string):
-    quantity_integer = int(quantity_string)
-    return quantity_integer
-
-
 
 def clean_brand(brand_str):
     brands = ["Einstein's", "Kraft", "Bob's Red Mill", "Delish", "Kroger", "V8", "Campbell's", "Kikkoman", "Del Monte", "Farberware", "Pam", "McCormick","Chateau Bonnet"]
     brand_id = brands.index(brand_str)
     return brand_id
 
+def clean_id(id_str, options):
+    try:
+        product_id = int(id_str)
+    except ValueError:
+        input('''
+                \n *** ID ERROR *** 
+                \n The ID should be a number  
+                \rPress enter to try again''')
+        return
+    else:
+        if product_id in options:
+            return product_id
+        else:
+            print(f'''
+                \n *** ID ERROR *** 
+                \n The ID should be within the optionS
+                ''')
+            return
+
+
 
 # ****************************** READ CVS FILES **************************************
+
+
 def add_csv():
     with open('brands.csv') as csvfile:
         data_brands = csv.reader(csvfile)
@@ -84,7 +120,7 @@ def add_csv():
                 date_updated=date_updated, 
                 brand_id=brand_id) 
                 session.add(new_product)
-        session.commit()
+    session.commit()
 
 # ****************************** APP FUNCTION   **************************************
 
@@ -93,24 +129,59 @@ def app():
     while app_running:
         choice = menu()
         if choice == "N":
-            product_name = input("What is the name of the product? " )
-            product_quantity = input("How many products are available? " )
-            product_price = input("How much is the product? " )
-            # # for brand in brands
-            # #     print(f'{brand})
-            brand_id = input('''
+            name = input("What is the name of the product? " )
+
+            quantity_error = True
+            while quantity_error:
+                quantity = input("How many products are available? " )
+                quantity = clean_quantity(quantity)
+                if type(quantity)  == int:
+                    quantity_error = False
+
+            price_error = True
+            while price_error: 
+                price = input("How much is the product? (Ex: $25.64) - " )
+                price = clean_price(price)
+                if type(price) == int:
+                    price_error = False
+
+            date =  datetime.datetime.now().strftime('%m/%d/%Y/')
+            date = clean_date(date)
+            brandId = input('''
             \rPlease select a brand from the following:
             \r1) Einstein's
             \r2) Kraft
             \r3) Bob's Red Mill
             \r4) Delish 
             \rPlease choose the number for the corresponding brand or press X if the brand is not listed -  ''')
-            print(f'\n*** {product_name} has been added!***')
-            pass
+            if brandId in ['1','2', '3', '4', '5', '6','7', '8', '9', '10', ]:
+                print(f'\n*** {name} has been added!***')
+            else:
+                print(f'\n*** no Brand added!***')
+
         elif choice == "V":
-            product_id = input("Enter a product's ID number. " )
-            # View a Product by ID
-            pass
+            # for product in session.query(Product):
+            #     print(f'\n{product.product_id} | {product.product_name} | ${product.product_price/100}')
+            # input('\nPress enter to return to the main menu.')
+
+            id_options = []
+            for product in session.query(Product):
+                id_options.append(product.product_id)
+            id_error = True
+            while id_error:
+                product_id = input(f'''
+               \rId Options are '{id_options}'
+              \n Enter a product's ID number: ''')
+                product_id = clean_id(product_id, id_options)
+                if type(product_id) == int:
+                    id_error = False
+            the_product = session.query(Product).filter(Product.product_id == product_id).first()
+            print(f'''
+                  \n{the_product.product_name} by {the_product.product_id}
+                  \rDate Updated {the_product.date_updated}
+                  \rCurrent Price: ${the_product.product_price/100}
+                  ''')
+        
         elif choice == "A":
             # Product Analysis
             print(f'\n The most expensive product is: {product_name}.')
@@ -130,10 +201,9 @@ def app():
 
 # ****************************** DUNDER DATA  **************************************
 
-
-
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    # app()
     add_csv()
+    app()
+   
     
